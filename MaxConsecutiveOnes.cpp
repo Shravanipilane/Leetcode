ad75bf26b867570f6451106821c4857a4407ce2d
Shravanipilane <shravanipilane6@gmail.com>
2026-01-18 22:47:06 +0530

    Print MaxConsecutiveOnes

--- MaxConsecutiveOnes.cpp
+++ MaxConsecutiveOnes.cpp
@@ -23,60 +23,9 @@ public:
         }
     }  
     return ans;
-    
+
     }
 };
 
 
 
-int cnt = 0;
-int ans = 0;
-for(int i=0;i<n;i++)
-{
-    if(nums[i] == 1)
-    {
-        cnt++;
-        ans = max(ans, cnt);
-    }
-    else
-    {
-        cnt = 0;
-    }
-}
-return ans;
-
-int n = nums.size();
-int cnt = 0;
-int ans = 0;
-
-for(i=0;i<n;i++)
-{
-    if(nums[i] == 1)
-    {
-        cnt++;
-        ans = max(ans, cnt);
-    }
-    else
-    {
-        cnt = 0;
-    }
-}
-return ans;
-
-
-
-int n = nums.size();
-int cnt = 0 ;
-int cnt = 0;
-for(i=0;i<n;i++)
-{
-    if(nums[i] == 1)
-    {
-        cnt++;
-        ans = max(ans, cnt);
-    }
-    else{
-        cnt = 0;
-    }
-
-}
