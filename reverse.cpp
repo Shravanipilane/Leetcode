d58c0ea5f83c31bcfe8a3f18171b6330fdb6cf8e
Shravanipilane <shravanipilane6@gmail.com>
2026-01-19 22:12:35 +0530

    Print reverse numbers

--- reverse.cpp
+++ reverse.cpp
@@ -17,30 +17,3 @@ class solution
 };
 
 
-int reverse(int x)
-{
-   int ans = 0;
-   int MAX = INT_MAX/10, MIN = INT_MIN/10;
-   while(x != 0)
-   {
-      if( MAX < ans || MIN > ans)
-          return 0;
-      ans = ans * 10 + (x % 10);
-      x /= 10;
-   }
-   return ans;
-
-}
-
-
-
-int ans = 0;
-int MAX = INT_MAX/10, MIN = INT_MIN/10;
-while(x != 0)
-{
-   if(MAX < ans || MIN > ans)
-      return 0;
-   ans = ans * 10 + (x % 10);
-   x /= 10;
-}
-return ans;
