608021212cd116f07d7b5797ac2919d28ab01607
Shravanipilane <shravanipilane6@gmail.com>
2026-05-23 23:50:41 +0530

    middle of the linked list

---
+++ MiddleofLL.cpp
@@ -0,0 +1,17 @@
+Class Solution
+{
+    public:
+    ListNode* middleNode(ListNode* head)
+    {
+        ListNode* slow = head;
+        ListNode* fast = head;
+
+        while (fast != NULL && fast->next != NULL)
+        {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+
+        return slow;
+    }
+}
