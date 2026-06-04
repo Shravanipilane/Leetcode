3d4b3797e6f2c216e7749f413219187faa99ec3c
Shravanipilane <shravanipilane6@gmail.com>
2026-06-04 23:11:14 +0530

    return island from the grid which id '1'.

---
+++ NoofIsland.cpp
@@ -0,0 +1,56 @@
+class solution
+{
+    public:
+    void markAllConnected(vector<vector<char>>& grid, int r, int c)
+    {
+        int rows = grid.size();
+        int cols = grid[0].size();
+
+        queue<pair<int, int>> q;
+        q.push({r, c});
+        grid[r][c] = '0'; 
+
+        int dr[] = {-1, 1, 0, 0};
+        int dc[] = {0, 0, 1, -1};
+
+        while(!q.empty())
+        {
+            auto [currR, currC] = q.front();
+            q.pop();
+
+            for(int k=0;k<4;k++)
+            {
+                int nr = r + dr[k];
+                int nc = c + dc[k];
+
+                if(nr >= 0 && nc >= 0 && nr < rows && nc < cols && grid[nr][nc] == '1')
+                {
+                    q.push({nr, nc});
+                    grid[nr][nc] = '0'; 
+                }
+            }
+        }
+    }
+}
+int numIslands(vector<vector<char>>& grid)
+{
+    int rows = grid.size();
+    int cols = grid[0].size();
+    int island = 0;
+ 
+    for(int r=0;r<rows;r++)
+    {
+        for(int c=0;c<cols;c++)
+        {
+            if(grid[r][c] == '1')
+            {
+                if(grid[r][c] == '1')
+                {
+                    island++;
+                    markAllConnected(grid, r, c);
+                }
+            }
+        }
+    }
+    return island;
+}
