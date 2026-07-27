998718d83ca2dc9d46a391616be533d78c7182f7
Shravanipilane <shravanipilane6@gmail.com>
2026-07-27 17:48:40 +0530

    valid anagram

--- Validanagram.cpp
+++ Validanagram.cpp
@@ -22,3 +22,31 @@ public:
         return true;
     }
 };
+
+
+
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+
+        if(s.length() != t.length())
+            return false;
+
+        int n = s.size();
+        int freqS[26] = {0};
+        int freqT[26] = {0};
+
+        for(char ch : s)
+            freqS[ch - 'a']++;
+        for(char ch : t)
+            freqT[ch - 'a']++;
+        for(int i=0;i<n;i++)
+        {
+            if(freqS[i] != freqT[i])
+                 return false;
+        }
+        
+
+        return true;
+    }
+};
